fd1712959e607d3b028ebd724e6e61307dc61eb5
SangHyouk Jin <chingoo2@naver.com>
2025-10-01 22:40:14 +0900

    default-update-timestamps-region 과 default-query-results-region 두 리전을 cacheManagerCustomizer 에서 생성하도록 추가 Hibernate Query Cache 켜면 반드시 필요한 영역이어서, 이제는 더 이상 "Missing cache" 경고나 CacheImplementor 초기화 실패가 발생하지 않음

--- src/main/java/com/daangcool/stack/config/CacheConfiguration.java
+++ src/main/java/com/daangcool/stack/config/CacheConfiguration.java
@@ -1,28 +1,27 @@
 package com.daangcool.stack.config;
 
-import java.net.URI;
-import java.util.concurrent.TimeUnit;
-import javax.cache.configuration.MutableConfiguration;
-import javax.cache.expiry.CreatedExpiryPolicy;
-import javax.cache.expiry.Duration;
-import org.hibernate.cache.jcache.ConfigSettings;
 import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.redisson.config.ClusterServersConfig;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 import org.redisson.jcache.configuration.RedissonConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.info.GitProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
-import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import tech.jhipster.config.JHipsterProperties;
 import tech.jhipster.config.cache.PrefixedKeyGenerator;
+
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
+import javax.cache.expiry.Duration;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -38,8 +37,9 @@ public class CacheConfiguration {
         URI redisUri = URI.create(jHipsterProperties.getCache().getRedis().getServer()[0]);
 
         Config config = new Config();
-        // Fix Hibernate lazy initialization https://github.com/jhipster/generator-jhipster/issues/22889
+        // Fix Hibernate lazy initialization
         config.setCodec(new org.redisson.codec.SerializationCodec());
+
         if (jHipsterProperties.getCache().getRedis().isCluster()) {
             ClusterServersConfig clusterServersConfig = config
                 .useClusterServers()
@@ -63,35 +63,40 @@ public class CacheConfiguration {
                 singleServerConfig.setPassword(redisUri.getUserInfo().substring(redisUri.getUserInfo().indexOf(':') + 1));
             }
         }
+
         jcacheConfig.setStatisticsEnabled(true);
         jcacheConfig.setExpiryPolicyFactory(
             CreatedExpiryPolicy.factoryOf(new Duration(TimeUnit.SECONDS, jHipsterProperties.getCache().getRedis().getExpiration()))
         );
-        return RedissonConfiguration.fromInstance(Redisson.create(config), jcacheConfig);
+
+        RedissonClient redisson = Redisson.create(config);
+        return RedissonConfiguration.fromInstance(redisson, jcacheConfig);
     }
 
     @Bean
     public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(javax.cache.CacheManager cm) {
-        return hibernateProperties -> hibernateProperties.put(ConfigSettings.CACHE_MANAGER, cm);
+        return hibernateProperties -> hibernateProperties.put(org.hibernate.cache.jcache.ConfigSettings.CACHE_MANAGER, cm);
     }
 
     @Bean
-    public JCacheManagerCustomizer cacheManagerCustomizer(javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration) {
+    public org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer cacheManagerCustomizer(javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration) {
         return cm -> {
+            // JHipster 기본 캐시
             createCache(cm, com.daangcool.stack.repository.UserRepository.USERS_BY_LOGIN_CACHE, jcacheConfiguration);
             createCache(cm, com.daangcool.stack.repository.UserRepository.USERS_BY_EMAIL_CACHE, jcacheConfiguration);
             createCache(cm, com.daangcool.stack.domain.User.class.getName(), jcacheConfiguration);
             createCache(cm, com.daangcool.stack.domain.Authority.class.getName(), jcacheConfiguration);
             createCache(cm, com.daangcool.stack.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+
+            // Hibernate Query Cache 기본 리전 추가
+            createCache(cm, "default-update-timestamps-region", jcacheConfiguration);
+            createCache(cm, "default-query-results-region", jcacheConfiguration);
+
             // jhipster-needle-redis-add-entry
         };
     }
 
-    private void createCache(
-        javax.cache.CacheManager cm,
-        String cacheName,
-        javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration
-    ) {
+    private void createCache(javax.cache.CacheManager cm, String cacheName, javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration) {
         javax.cache.Cache<Object, Object> cache = cm.getCache(cacheName);
         if (cache != null) {
             cache.clear();
