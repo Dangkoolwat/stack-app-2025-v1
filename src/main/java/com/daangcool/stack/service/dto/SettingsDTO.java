373f2ae9132fa6d39ed60fa4cd7d516f809ff565
SangHyouk Jin <chingoo2@naver.com>
2025-10-13 03:42:37 +0900

    -SeettingsDTO가 직렬화(Serializable)가 가능하지 않아서 발생하는 문제로 보입니다. 캐시(JCache with Redisson)에 객체를 저장하려면 해당 객체가 Serializable 인터페이스를 구현해야 합니다.

--- src/main/java/com/daangcool/stack/service/dto/SettingsDTO.java
+++ src/main/java/com/daangcool/stack/service/dto/SettingsDTO.java
@@ -5,13 +5,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 /**
  * A DTO for the {@link com.daangcool.stack.domain.Settings} entity.
  */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class SettingsDTO {
+public class SettingsDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @NotNull
     private Long tokenValiditySeconds;
