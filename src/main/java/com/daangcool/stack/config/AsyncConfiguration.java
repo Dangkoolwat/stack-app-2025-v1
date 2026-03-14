ee260ca33c77b114e243ca08b3af361e53cb679b
SangHyouk Jin <chingoo2@naver.com>
2026-03-14 19:55:19 +0900

    - ThreadPoolTaskExecutor 대신에 VirtualThreadTaskExecutor 사용

--- src/main/java/com/daangcool/stack/config/AsyncConfiguration.java
+++ src/main/java/com/daangcool/stack/config/AsyncConfiguration.java
@@ -4,15 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
-import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
+import org.springframework.boot.task.SimpleAsyncTaskExecutorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import tech.jhipster.async.ExceptionHandlingAsyncTaskExecutor;
 
 import java.util.concurrent.Executor;
 
@@ -24,26 +23,23 @@ public class AsyncConfiguration implements AsyncConfigurer {
 
     private static final Logger LOG = LoggerFactory.getLogger(AsyncConfiguration.class);
 
-    private final TaskExecutionProperties taskExecutionProperties;
+    private final AsyncTaskExecutor taskExecutor;
 
-    public AsyncConfiguration(TaskExecutionProperties taskExecutionProperties) {
-        this.taskExecutionProperties = taskExecutionProperties;
-    }
+        public AsyncConfiguration(SimpleAsyncTaskExecutorBuilder builder) {
+            this.taskExecutor = builder
+                .threadNamePrefix("async-")
+                .build();
+        }
 
-    @Override
-    @Bean(name = "taskExecutor")
-    public Executor getAsyncExecutor() {
-        LOG.debug("::Creating Async Task Executor");
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(taskExecutionProperties.getPool().getCoreSize());
-        executor.setMaxPoolSize(taskExecutionProperties.getPool().getMaxSize());
-        executor.setQueueCapacity(taskExecutionProperties.getPool().getQueueCapacity());
-        executor.setThreadNamePrefix(taskExecutionProperties.getThreadNamePrefix());
-        return new ExceptionHandlingAsyncTaskExecutor(executor);
-    }
+        @Override
+        @Bean(name = "taskExecutor")
+        public Executor getAsyncExecutor() {
+            LOG.debug("::: Creating Virtual-Thread Async Task Executor");
+            return taskExecutor;
+        }
 
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new SimpleAsyncUncaughtExceptionHandler();
+        @Override
+        public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+            return new SimpleAsyncUncaughtExceptionHandler();
+        }
     }
-}
