eee24a3b27810d4b98c628c433348c97c10ee908
SangHyouk Jin <chingoo2@naver.com>
2026-03-20 12:54:20 +0900

    - 로그인 레이아웃 수정

--- src/main/webapp/app/account/login-form/login-form.vue
+++ src/main/webapp/app/account/login-form/login-form.vue
@@ -1,59 +1,62 @@
 <template>
-  <div class="d-flex justify-content-center">
-    <div class="row-md">
-      <div class="col-md-12">
-        <b-alert
-          data-cy="loginError"
-          variant="danger"
-          :model-value="authenticationError"
-          v-html="t$('login.messages.error.authentication')"
-        ></b-alert>
-      </div>
-      <div class="col-md-12">
-        <b-form @submit.prevent="doLogin()">
-          <b-form-group :label="t$('global.form[\'username.label\']')" label-for="username">
-            <b-form-input
-              id="username"
-              type="text"
-              name="username"
-              :placeholder="t$('global.form[\'username.placeholder\']')"
-              v-model="login"
-              data-cy="username"
-            >
-            </b-form-input>
-          </b-form-group>
-          <b-form-group :label="t$('login.form.password')" label-for="password">
-            <b-form-input
-              id="password"
-              type="password"
-              name="password"
-              :placeholder="t$('login.form[\'password.placeholder\']')"
-              v-model="password"
-              data-cy="password"
-            >
-            </b-form-input>
-          </b-form-group>
+  <div class="login-form-wrap">
+    <div class="login-card">
+      <b-alert
+        data-cy="loginError"
+        variant="danger"
+        :model-value="authenticationError"
+        v-html="t$('login.messages.error.authentication')"
+      ></b-alert>
+
+      <b-form @submit.prevent="doLogin()">
+        <b-form-group :label="t$('global.form[\'username.label\']')" label-for="username" class="mb-3">
+          <b-form-input
+            id="username"
+            type="text"
+            name="username"
+            :placeholder="t$('global.form[\'username.placeholder\']')"
+            v-model="login"
+            data-cy="username"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group :label="t$('login.form.password')" label-for="password" class="mb-3">
+          <b-form-input
+            id="password"
+            type="password"
+            name="password"
+            :placeholder="t$('login.form[\'password.placeholder\']')"
+            v-model="password"
+            data-cy="password"
+          ></b-form-input>
+        </b-form-group>
+
+        <div class="login-card__options">
           <b-form-checkbox id="rememberMe" name="rememberMe" v-model="rememberMe" checked>
             <span>{{ t$('login.form.rememberme') }}</span>
           </b-form-checkbox>
-          <div>
-            <b-button data-cy="submit" type="submit" variant="primary">{{ t$('login.form.button') }}</b-button>
-          </div>
-        </b-form>
-        <p></p>
-        <div>
-          <b-alert :model-value="true" variant="warning">
-            <b-link :to="'/account/reset/request'" class="alert-link" data-cy="forgetYourPasswordSelector">{{
-              t$('login.password.forgot')
-            }}</b-link>
-          </b-alert>
         </div>
-        <div>
-          <b-alert :model-value="true" variant="warning">
-            <span>{{ t$('global.messages.info.register.noaccount') }}</span>
-            <b-link :to="'/register'" class="alert-link">{{ t$('global.messages.info.register.link') }}</b-link>
-          </b-alert>
+
+        <div class="d-grid mt-3">
+          <b-button data-cy="submit" type="submit" variant="primary" class="w-100">
+            {{ t$('login.form.button') }}
+          </b-button>
         </div>
+      </b-form>
+
+      <div class="login-links mt-4">
+        <b-alert :model-value="true" variant="light" class="login-links__item">
+          <b-link :to="'/account/reset/request'" class="alert-link" data-cy="forgetYourPasswordSelector">
+            {{ t$('login.password.forgot') }}
+          </b-link>
+        </b-alert>
+
+        <b-alert :model-value="true" variant="light" class="login-links__item">
+          <span>{{ t$('global.messages.info.register.noaccount') }}</span>
+          <b-link :to="'/register'" class="alert-link">
+            {{ t$('global.messages.info.register.link') }}
+          </b-link>
+        </b-alert>
       </div>
     </div>
   </div>
