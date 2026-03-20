1497ad094a1d540167b4247623a93097a8a5c4ae
SangHyouk Jin <chingoo2@naver.com>
2026-03-20 12:30:59 +0900

    - 글로벌 설정을 통해 모든 페이징 결과가 자동으로 표준화된 DTO(PagedModel) 형식으로 직렬화되도록 수정하

--- src/main/java/com/daangcool/stack/config/DatabaseConfiguration.java
+++ src/main/java/com/daangcool/stack/config/DatabaseConfiguration.java
@@ -3,10 +3,15 @@ package com.daangcool.stack.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @Configuration
 @EnableJpaRepositories({ "com.daangcool.stack.repository" })
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableTransactionManagement
+
 public class DatabaseConfiguration {}
