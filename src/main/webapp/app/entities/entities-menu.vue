107fae20d5e8bdbf3a8aad5320c51f706951fc96
SangHyouk Jin <chingoo2@naver.com>
2026-03-20 12:53:48 +0900

    - 관리자 페이지 엔티티 항목 추가

--- src/main/webapp/app/entities/entities-menu.vue
+++ src/main/webapp/app/entities/entities-menu.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
+    <b-dropdown-item to="/settings">
+      <font-awesome-icon icon="cogs" />
+      <span class="ms-1" v-text="t$('stackApp.settings.home.createOrEditLabel')"></span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/common-code-group">
+      <font-awesome-icon icon="layer-group" />
+      <span class="ms-1" v-text="t$('entities.commonCodeGroup.menu')"></span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/common-code-detail">
+      <font-awesome-icon icon="list-ul" />
+      <span class="ms-1" v-text="t$('entities.commonCodeDetail.menu')"></span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/tag">
+      <font-awesome-icon icon="hashtag" />
+      <span class="ms-1" v-text="t$('entities.tag.menu')"></span>
+    </b-dropdown-item>
+    <b-dropdown-item to="/board">
+      <font-awesome-icon icon="file-alt" />
+      <span class="ms-1" v-text="t$('entities.board.menu')"></span>
+    </b-dropdown-item>
+
     <!-- jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here -->
+
+
+
+
   </div>
 </template>
 
--- src/main/webapp/app/router/entities.ts
+++ src/main/webapp/app/router/entities.ts
@@ -6,6 +6,76 @@ export default {
   path: '/',
   component: Entities,
   children: [
+    {
+      path: 'board',
+      name: 'Board',
+      component: () => import('@/entities/board/board.vue'),
+    },
+    {
+      path: 'board/new',
+      name: 'BoardCreate',
+      component: () => import('@/entities/board/board-update.vue'),
+    },
+    {
+      path: 'board/:id/edit',
+      name: 'BoardEdit',
+      component: () => import('@/entities/board/board-update.vue'),
+    },
+    {
+      path: 'board/:id/view',
+      name: 'BoardView',
+      component: () => import('@/entities/board/board-detail.vue'),
+    },
+    {
+      path: 'common-code-group',
+      name: 'CommonCodeGroup',
+      component: () => import('@/entities/common-code-group/common-code-group.vue'),
+    },
+    {
+      path: 'common-code-group/new',
+      name: 'CommonCodeGroupCreate',
+      component: () => import('@/entities/common-code-group/common-code-group-update.vue'),
+    },
+    {
+      path: 'common-code-group/:groupCode/edit',
+      name: 'CommonCodeGroupEdit',
+      component: () => import('@/entities/common-code-group/common-code-group-update.vue'),
+    },
+    {
+      path: 'common-code-group/:groupCode/view',
+      name: 'CommonCodeGroupView',
+      component: () => import('@/entities/common-code-group/common-code-group-detail.vue'),
+    },
+    {
+      path: 'tag',
+      name: 'Tag',
+      component: () => import('@/entities/tag/tag.vue'),
+    },
+    {
+      path: 'common-code-detail',
+      name: 'CommonCodeDetail',
+      component: () => import('@/entities/common-code-detail/common-code-detail.vue'),
+    },
+    {
+      path: 'common-code-detail/new',
+      name: 'CommonCodeDetailCreate',
+      component: () => import('@/entities/common-code-detail/common-code-detail-update.vue'),
+    },
+    {
+      path: 'common-code-detail/:id/edit',
+      name: 'CommonCodeDetailEdit',
+      component: () => import('@/entities/common-code-detail/common-code-detail-update.vue'),
+    },
+    {
+      path: 'settings',
+      name: 'Settings',
+      component: () => import('@/entities/settings/settings.vue'),
+    },
+
     // jhipster-needle-add-entity-to-router - JHipster will add entities to the router here
+
+
+
+
   ],
 };
